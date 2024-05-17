6e11aa207d6d61528248cd59e2ef8a6cdab7d995
Hosein Eftekhari Manesh <h.eftekhar.m@gmail.com>
2024-05-17 23:57:31 +0330

    Do some cleanup

--- src/main/java/com/hfm/saghi/SaghiProgressBar.java
+++ src/main/java/com/hfm/saghi/SaghiProgressBar.java
@@ -17,21 +17,11 @@ import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.geom.RoundRectangle2D;
 
-public class SaghiProgressBar extends BasicProgressBarUI{
+public class SaghiProgressBar extends BasicProgressBarUI {
 
-    private String verses =  PoemsRepository.INSTANCE.getVerse(); ;
-
-   /* private Font font;
-    {
-        try {
-            font = Font.createFont(Font.PLAIN, Objects.requireNonNull(getClass().getResourceAsStream("/font.ttf"))).deriveFont(20f);
-        } catch (FontFormatException | IOException e) {
-            throw new RuntimeException(e);
-        }
-    }*/
-
-
+    private String verses = PoemsRepository.INSTANCE.getVerse();
     private final Color wineColor = new Color(114, 47, 55);
+    private Font font = null;
 
 
     @SuppressWarnings({"MethodOverridesStaticMethodOfSuperclass", "UnusedDeclaration"})
@@ -42,7 +32,7 @@ public class SaghiProgressBar extends BasicProgressBarUI{
 
     @Override
     public Dimension getPreferredSize(JComponent c) {
-        return new Dimension(super.getPreferredSize(c).width, JBUI.scale(20));
+        return new Dimension(super.getPreferredSize(c).width, JBUIScale.scale(20));
     }
 
     @Override
@@ -63,14 +53,14 @@ public class SaghiProgressBar extends BasicProgressBarUI{
 
     private volatile int offset = 0;
     private final int velocity = 2;
+
     @Override
     protected void paintIndeterminate(Graphics g2d, JComponent c) {
 
         if (!(g2d instanceof Graphics2D)) {
             return;
         }
-        Graphics2D g = (Graphics2D)g2d;
-
+        Graphics2D g = (Graphics2D) g2d;
 
         Insets b = progressBar.getInsets(); // area for border
         int barRectWidth = progressBar.getWidth() - (b.right + b.left);
@@ -90,7 +80,6 @@ public class SaghiProgressBar extends BasicProgressBarUI{
 
         g.setColor(new JBColor(Gray._165.withAlpha(50), Gray._88.withAlpha(50)));
         final GraphicsConfig config = GraphicsUtil.setupAAPainting(g);
-        g.translate(0, (c.getHeight() - h) / 2);
 
         final float R = JBUIScale.scale(8f);
         final float R2 = JBUIScale.scale(9f);
@@ -99,16 +88,18 @@ public class SaghiProgressBar extends BasicProgressBarUI{
         g.setColor(progressBar.getForeground());
         g.fill(new RoundRectangle2D.Float(0, 0, w - off, h - off, R2, R2));
         g.setColor(background);
-        g.fill(new RoundRectangle2D.Float(off, off, w - 2f*off - off, h - 2f*off - off, R, R));
-
-        g.setFont(progressBar.getFont().deriveFont(Font.PLAIN ,JBUIScale.scale(10f) ));
+        g.fill(new RoundRectangle2D.Float(off, off, w - 2f * off - off, h - 2f * off - off, R, R));
+        if (font == null) {
+            progressBar.getFont().deriveFont(Font.PLAIN, JBUIScale.scale(10f));
+        }
+        g.setFont(font);
         String stringVerse = verses;
         int stringWidth = g.getFontMetrics().stringWidth(stringVerse);
         offset += velocity;
-        offset %= (w +  stringWidth + 13);
+        offset %= (w + stringWidth + 13);
         g.setColor(getSelectionForeground());
         BasicGraphicsUtils.drawString(progressBar, g, stringVerse,
-                offset - stringWidth, 13);
+                offset - stringWidth, JBUIScale.scale(13));
         config.restore();
     }
 
@@ -141,7 +132,7 @@ public class SaghiProgressBar extends BasicProgressBarUI{
         Color background = parent != null ? parent.getBackground() : UIUtil.getPanelBackground();
 
         g.setColor(background);
-        Graphics2D g2 = (Graphics2D)g;
+        Graphics2D g2 = (Graphics2D) g;
         if (c.isOpaque()) {
             g.fillRect(0, 0, w, h);
         }
@@ -150,19 +141,13 @@ public class SaghiProgressBar extends BasicProgressBarUI{
         final float R2 = JBUIScale.scale(9f);
         final float off = JBUIScale.scale(1f);
 
-        g2.translate(0, (c.getHeight() - h)/2);
         g2.setColor(progressBar.getForeground());
         g2.fill(new RoundRectangle2D.Float(0, 0, w - off, h - off, R2, R2));
         g2.setColor(background);
-        g2.fill(new RoundRectangle2D.Float(off, off, w - 2f*off - off, h - 2f*off - off, R, R));
-//        g2.setColor(progressBar.getForeground());
-
-
+        g2.fill(new RoundRectangle2D.Float(off, off, w - 2f * off - off, h - 2f * off - off, R, R));
         g2.setColor(wineColor);
 
-        //NyanIcons.CAT_ICON.paintIcon(progressBar, g2, amountFull - JBUI.scale(10), -JBUI.scale(6));
-        g2.fill(new RoundRectangle2D.Float(2f*off,2f*off, amountFull - JBUI.scale(5f), h - JBUI.scale(5f), JBUI.scale(7f), JBUI.scale(7f)));
-        g2.translate(0, -(c.getHeight() - h)/2);
+        g2.fill(new RoundRectangle2D.Float(2f * off, 2f * off, amountFull - JBUIScale.scale(5f), h - JBUIScale.scale(5f), JBUIScale.scale(7f), JBUIScale.scale(7f)));
 
         // Deal with possible text painting
         if (progressBar.isStringPainted()) {
@@ -178,9 +163,6 @@ public class SaghiProgressBar extends BasicProgressBarUI{
         return availableLength;
     }
 
-    private int getPeriodLength() {
-        return JBUI.scale(16);
-    }
 
     private static boolean isEven(int value) {
         return value % 2 == 0;
